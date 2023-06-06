0d317ba84fbed49e2b3d62f8a2626159f6b6fdae
war <thomas@warwaris.at>
2023-06-06 10:24:02 +0200

    testing

--- build_gitlab.sh
+++ build_gitlab.sh
@@ -4,3 +4,5 @@ bash build.sh
 
 mkdir -p kit
 cp -a shibboleth-embedded-ds-patched-1.2.2 kit
+
+sha256sum shibboleth-embedded-ds-patched-1.2.2.tar.gz > shibboleth-embedded-ds-patched-1.2.2.tar.gz.sha256
